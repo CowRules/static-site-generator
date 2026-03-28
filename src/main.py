5dbf098e9e6794d02f7cac61dff393bb0942d070
Rosvaldas Atstupėnas <atstupenas.rosvaldas@gmail.com>
2026-03-28 19:37:58 +0200

    Make changes for github pages

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from textnode import TextNode, TextType
 import os
 import shutil
+import sys
 from utils import markdown_to_html_node, extract_title
 
 def static_to_public(source, destination):
     root = os.path.abspath(".")
     source_path = os.path.join(root, source)
     destination_path = os.path.join(root, destination)
-    if destination == "public":
+    if destination == "docs":
         shutil.rmtree(destination_path)
         os.mkdir(destination_path)
     for file in os.listdir(source_path):
@@ -19,7 +20,7 @@ def static_to_public(source, destination):
         else:
             shutil.copy(source_file, destination_file)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     source_content = ""
@@ -32,12 +33,12 @@ def generate_page(from_path, template_path, dest_path):
     html_content = markdown_to_html_node(source_content).to_html()
     title = extract_title(source_content)
     template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
-    print(template_content)
+    template_content = template_content.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs("/".join(dest_path.split("/")[:-1]), exist_ok=True)
     with open(dest_path, "w") as file:
         file.write(template_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     root = os.path.abspath(".")
     source_path = os.path.join(root, dir_path_content)
     destination_path = os.path.join(root, dest_dir_path)
@@ -46,12 +47,13 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         destination_file = os.path.join(destination_path, file)
         if os.path.isdir(source_file):
             #os.makedirs(destination_file, exist_ok=True)
-            generate_pages_recursive(f"{dir_path_content}/{file}", template_path, f"{dest_dir_path}/{file}")
+            generate_pages_recursive(f"{dir_path_content}/{file}", template_path, f"{dest_dir_path}/{file}", basepath)
         else:
-            generate_page(f"{dir_path_content}/{file}", template_path, f"{dest_dir_path}/{file.replace(".md", ".html")}")
+            generate_page(f"{dir_path_content}/{file}", template_path, f"{dest_dir_path}/{file.replace(".md", ".html")}", basepath)
 
 def main():
-    static_to_public("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    static_to_public("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
